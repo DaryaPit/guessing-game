9539c490fa3d1a6596cfc5cf746bc25557422a81
DaryaPit <m-dv@tut.by>
2022-08-01 13:44:05 +0300

    init:game

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor(left, right) {
+        this.left = 0;
+        this.right = 0;
+        this.mid = 0
+    }
 
     setRange(min, max) {
-
+        this.left = min;
+        this.right = max;
     }
 
     guess() {
-
+        this.mid = Math.ceil((this.left + this.right) / 2);        
+        return this.mid;
     }
 
     lower() {
-
+        this.right = this.mid;
     }
 
     greater() {
-
+        this.left = this.mid;
     }
 }
 
